1e4b90096adc64a98354091f98d1a075da21eff2
Kakashi <38396843+Kakashi010@users.noreply.github.com>
2021-08-16 09:26:19 +0600

    Update alert.py

--- alert.py
+++ alert.py
@@ -10,21 +10,21 @@ token='123456789'
 bot = telebot.TeleBot(token)
 
 #print (sys.argv[1]) соединение
-#print (sys.argv[2]) после перезагрузки / перезагрузка / проверка ноды
-#print (sys.argv[3]) имя ноды
-#print (sys.argv[4]) высота блока ноды
-#print (sys.argv[5]) высота блока сети
-#print (sys.argv[6]) аптайм
-#print (sys.argv[7]) пиры
-#print (sys.argv[8]) статус
-#print (sys.argv[9]) следующий блок
+#print (sys.argv[2]) After restart / Restart / проверка ноды
+#print (sys.argv[3]) Node name
+#print (sys.argv[4]) Node Block Height
+#print (sys.argv[5]) Network Block Height
+#print (sys.argv[6]) Uptime
+#print (sys.argv[7]) Peers
+#print (sys.argv[8]) Status
+#print (sys.argv[9]) Next Block
 
 if sys.argv[1] == 'succeeded':
 	if sys.argv[2] == 'info':
-		msg = bot.send_message(tgid, 'Нода: ' + sys.argv[3] + '\nВысота блока ноды: ' + sys.argv[4] + '\nВысота блока сети: ' + sys.argv[5] + '\nАптайм: ' + sys.argv[6] + '\nПиры: ' + sys.argv[7] + '\nСтатус: ' + sys.argv[8] + '\nСледующий блок: ' + sys.argv[9] + '')
+		msg = bot.send_message(tgid, 'Node: ' + sys.argv[3] + '\nNode Block Height: ' + sys.argv[4] + '\nNetwork Block Height: ' + sys.argv[5] + '\nUptime: ' + sys.argv[6] + '\nPeers: ' + sys.argv[7] + '\nStatus: ' + sys.argv[8] + '\nNext Block: ' + sys.argv[9] + '')
 	elif sys.argv[2] == 'canBeRestarted':
-		msg = bot.send_message(tgid, 'Нода: ' + sys.argv[3] + '\nВысота блока ноды: ' + sys.argv[4] + '\nВысота блока сети: ' + sys.argv[5] + '\nАптайм: ' + sys.argv[6] + '\nПиры: ' + sys.argv[7] + '\nСтатус: ' + sys.argv[8] + '\nСледующий блок: ' + sys.argv[9] + '\nВыполняется перезагрузка...')
+		msg = bot.send_message(tgid, 'Node: ' + sys.argv[3] + '\nNode Block Height: ' + sys.argv[4] + '\nNetwork Block Height: ' + sys.argv[5] + '\nUptime: ' + sys.argv[6] + '\nPeers: ' + sys.argv[7] + '\nStatus: ' + sys.argv[8] + '\nNext Block: ' + sys.argv[9] + '\nRestart in progress...')
 	elif sys.argv[2] == 'cantBeRestarted':
-		msg = bot.send_message(tgid, 'Нода: ' + sys.argv[3] + '\nВысота блока ноды: ' + sys.argv[4] + '\nВысота блока сети: ' + sys.argv[5] + '\nАптайм: ' + sys.argv[6] + '\nПиры: ' + sys.argv[7] + '\nСтатус: ' + sys.argv[8] + '\nСледующий блок: ' + sys.argv[9] + '\nДо блока менее 1ч, перезагрузка запрещена. Проверь ноду!')
+		msg = bot.send_message(tgid, 'Node: ' + sys.argv[3] + '\nNode Block Height: ' + sys.argv[4] + '\nNetwork Block Height: ' + sys.argv[5] + '\nUptime: ' + sys.argv[6] + '\nPeers: ' + sys.argv[7] + '\nStatus: ' + sys.argv[8] + '\nNext Block: ' + sys.argv[9] + '\nLess than 1hr to the block, Restart is retricted. Check Node!')
 else:
-	msg = bot.send_message(tgid, 'Нет соединения с нодой ' + sys.argv[2] + '. Возможно нода перезагружается.')
+	msg = bot.send_message(tgid, 'No connection to the node ' + sys.argv[2] + '. Probably node is restarting.')
